13c646a866db2d1b5c3dee25149eeb7d59c072dc
Aymeric PINEAU <ay.pineau@gmail.com>
2026-04-08 01:06:01 -0600

    fix: remove rollup plugin analyser

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,9 @@
-import { visualizer } from 'rollup-plugin-visualizer';
-import { defineConfig } from 'vite';
-import { intlayer, intlayerProxy } from 'vite-intlayer';
+import { defineConfig } from "vite";
+import { intlayer, intlayerProxy } from "vite-intlayer";
 
 export default defineConfig({
   plugins: [
     intlayerProxy(), // should be placed first
     intlayer(),
-    visualizer({
-      emitFile: true,
-      filename: 'stats.html',
-    }),
   ],
 });
